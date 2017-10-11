d4b258d12ff7316b299a2a020daf3e9de4775453
Tim <tja2353@gmail.com>
2017-10-11 11:16:04 +0100

    move service worker check to MyApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,54 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, ToastController } from 'ionic-angular';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
   constructor(
     platform: Platform,
-    ) {
+    private toastCtrl: ToastController,
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      this.serviceWorkerUpdatesCheck();
     });
+  }
+
+  private serviceWorkerUpdatesCheck(): void {
+    // listen to the service worker promise in index.html to see if there has been a new update.
+    // condition: the service-worker.js needs to have some kind of change - e.g. increment CACHE_VERSION.
+    window['isUpdateAvailable']
+      .then(isAvailable => {
+        if (isAvailable) {
+          console.log('isUpdateAvailable');
+          // window.location.reload();
+
+          const toast = this.toastCtrl.create({
+            message: 'A new version is available, reload ',
+            closeButtonText: "Reload",
+            showCloseButton: true,
+          });
+          toast.onDidDismiss((data, role) => {
+            if (role === 'close') {
+              window.location.reload();
+            };
+          });
+          toast.present();
+          /*        
+                  const toast = this.toastCtrl.create({
+                    message: 'New Update available! Reload the webapp to see the latest juicy changes.',
+                    position: 'bottom',
+                    showCloseButton: true,
+                  });
+                  toast.present();
+           */
+        }
+      });
   }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, ToastController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -9,29 +9,6 @@ export class HomePage {
 
   constructor(
     public navCtrl: NavController,
-    private toastCtrl: ToastController,
   ) {
-
-  }
-
-  ngOnInit() {
-    // listen to the service worker promise in index.html to see if there has been a new update.
-    // condition: the service-worker.js needs to have some kind of change - e.g. increment CACHE_VERSION.
-    window['isUpdateAvailable']
-      .then(isAvailable => {
-        if (isAvailable) {
-          console.log('isUpdateAvailable');
-          // window.location.reload();
-         
-          
-          const toast = this.toastCtrl.create({
-            message: 'New Update available! Reload the webapp to see the latest juicy changes.',
-            position: 'bottom',
-            showCloseButton: true,
-          });
-          toast.present();
-          
-        }       
-      });      
-  }  
+  } 
 }
